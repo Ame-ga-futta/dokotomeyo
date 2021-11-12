d3777f48fa166f86310b51edfcda4abeeb0be44a
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-11-12 16:37:40 +0900

    about コンポーネント化

--- app/javascript/components/Menu.jsx
+++ app/javascript/components/Menu.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
 import styled from 'styled-components';
+import About from "./About"
 
 const Menu = () => {
   const [openMenu, setOpenMenu] = useState(false);
   const hamburger = () => {
     setOpenMenu(!openMenu);
+  };
+
+  const [openAbout, setOpenAbout] = useState(false);
+  const about = () => {
+    setOpenMenu(false);
+    setOpenAbout(true);
   };
 
   return (
@@ -19,9 +26,10 @@ const Menu = () => {
           <li><a href="#">ログイン</a></li>
           <li><a href="#">新規登録</a></li>
           <li><a href="#">駐車場情報投稿</a></li>
-          <li><a href="#">dokotomeyoとは</a></li>
+          <li><p onClick={about}>dokotomeyoとは</p></li>
         </ul>
       </SGlobalMenuSp>
+      <About openAbout={openAbout} setOpenAbout={setOpenAbout} />
     </>
   );
 };
@@ -99,6 +107,12 @@ const SGlobalMenuSp = styled.nav`
     transition: all 0.6s;
   }
   ul li a {
+    display: block;
+    color: #fff;
+    padding: 1em 0;
+    text-decoration :none;
+  }
+  ul li p {
     display: block;
     color: #fff;
     padding: 1em 0;
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   namespace :dokotomeyo do
     get "/",                      to: "top#top"
     get :sign_up,                 to: "top#sign_up_form"
-    get :login,              to: "top#login_form"
-    get :post,               to: "top#post_form"
-    get :about,                   to: "top#about"
+    get :login,                   to: "top#login_form"
+    get :post,                    to: "top#post_form"
   end
 end
