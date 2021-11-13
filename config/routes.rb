d6f00db8604e7fd753d54ab6dd68142cf3e187c0
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-11-13 10:18:40 +0900

    見た目少し変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :dokotomeyo do
     get "/",                      to: "top#top"
-    get :sign_up,                 to: "top#sign_up_form"
-    get :login,                   to: "top#login_form"
     get :post,                    to: "top#post_form"
+    get :login,                   to: "top#login_form"
+    get :sign_up,                 to: "top#sign_up_form"
   end
 end
