6922d16dc477fe530783ffbeba9a282a88690114
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-12-04 17:07:39 +0900

    rubocop対応

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,7 @@ RSpec.describe User, type: :model do
     let(:new_user) { User.new(params) }
 
     context "with username, email and password" do
-      let(:params) { {
-        name: "new_user",
-        email: "new_user@gmail.com",
-        password: "password"
-      } }
+      let(:params) { { name: "new_user", email: "new_user@gmail.com", password: "password" } }
 
       it "is valid with username, email and password" do
         expect(new_user).to be_valid
@@ -15,11 +11,7 @@ RSpec.describe User, type: :model do
     end
 
     context "without Username" do
-      let(:params) { {
-        name: "",
-        email: "new_user@gmail.com",
-        password: "password"
-      } }
+      let(:params) { { name: "", email: "new_user@gmail.com", password: "password" } }
 
       it "is invalid without Username" do
         expect(new_user).not_to be_valid
@@ -27,11 +19,7 @@ RSpec.describe User, type: :model do
     end
 
     context "without email" do
-      let(:params) { {
-        name: "new_user",
-        email: "",
-        password: "password"
-      } }
+      let(:params) { { name: "new_user", email: "", password: "password" } }
 
       it "is invalid without email" do
         expect(new_user).not_to be_valid
@@ -39,11 +27,7 @@ RSpec.describe User, type: :model do
     end
 
     context "without password" do
-      let(:params) { {
-        name: "new_user",
-        email: "new_user@gmail.com",
-        password: ""
-      } }
+      let(:params) { { name: "new_user", email: "new_user@gmail.com", password: "" } }
 
       it "is invalid without password" do
         expect(new_user).not_to be_valid
@@ -52,11 +36,7 @@ RSpec.describe User, type: :model do
 
     context "the length of password is not over 8" do
       context "the length of password is 7" do
-        let(:params) { {
-          name: "new_user",
-          email: "new_user@gmail.com",
-          password: "passwor"
-        } }
+        let(:params) { { name: "new_user", email: "new_user@gmail.com", password: "passwor" } }
 
         it "is invalid when the length of password is 7" do
           expect(new_user).not_to be_valid
@@ -64,11 +44,7 @@ RSpec.describe User, type: :model do
       end
 
       context "the length of password is 8" do
-        let(:params) { {
-          name: "new_user",
-          email: "new_user@gmail.com",
-          password: "password"
-        } }
+        let(:params) { { name: "new_user", email: "new_user@gmail.com", password: "password" } }
 
         it "is valid when the length of password is 8" do
           expect(new_user).to be_valid
@@ -78,11 +54,7 @@ RSpec.describe User, type: :model do
 
     context "the length of name is over 20" do
       context "the length of name is 20" do
-        let(:params) { {
-          name: "new_userrrrrrrrrrrrr",
-          email: "new_user@gmail.com",
-          password: "password"
-        } }
+        let(:params) { { name: "new_userrrrrrrrrrrrr", email: "new_user@gmail.com", password: "password" } }
 
         it "is invalid when the length of name is 20" do
           expect(new_user).to be_valid
@@ -90,11 +62,7 @@ RSpec.describe User, type: :model do
       end
 
       context "the length of name is 21" do
-        let(:params) { {
-          name: "new_userrrrrrrrrrrrrr",
-          email: "new_user@gmail.com",
-          password: "password"
-        } }
+        let(:params) { { name: "new_userrrrrrrrrrrrrr", email: "new_user@gmail.com", password: "password" } }
 
         it "is invalid when the length of name is 21" do
           expect(new_user).not_to be_valid
@@ -108,11 +76,7 @@ RSpec.describe User, type: :model do
         email: "existing_user@gmail.com",
         password: "password"
       )
-      let(:params) { {
-        name: "new_user",
-        email: "existing_user@gmail.com",
-        password: "password"
-      } }
+      let(:params) { { name: "new_user", email: "existing_user@gmail.com", password: "password" } }
 
       it "is invalid if email is duplicated" do
         expect(new_user).not_to be_valid
