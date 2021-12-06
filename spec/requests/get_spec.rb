25062fb79f8e22394c839cdaeff41c0079912cd5
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-12-06 13:47:09 +0900

    リクエストスペック作成

---
+++ spec/requests/get_spec.rb
@@ -0,0 +1,45 @@
+RSpec.describe "get", type: :request do
+  describe "GET" do
+    context "root_path" do
+      it "root response is 200" do
+        get root_path
+        expect(response).to redirect_to(dokotomeyo_path)
+      end
+    end
+
+    context "dokotomeyo_path" do
+      it "top response is 200" do
+        get dokotomeyo_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "dokotomeyo_mypage_path" do
+      it "mypage response is 200" do
+        get dokotomeyo_mypage_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "dokotomeyo_post_path" do
+      it "post response is 200" do
+        get dokotomeyo_post_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "dokotomeyo_signup_path" do
+      it "signup response is 200" do
+        get dokotomeyo_signup_path
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "dokotomeyo_login_path" do
+      it "login response is 200" do
+        get dokotomeyo_login_path
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
