8187a1c668af4f2b8114b21b57f207255365bdcf
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-12-07 18:13:43 +0900

    parkings_post_spec.rb 失敗パターン書き忘れ

--- spec/requests/parkings_post_spec.rb
+++ spec/requests/parkings_post_spec.rb
@@ -14,5 +14,20 @@ RSpec.describe "Parkings_post", type: :request do
       }
       expect(JSON.parse(response.body)["status"]).to eq 200
     end
+
+    it "parking responce is 400" do
+      new_parking = FactoryBot.build(:parking)
+      post dokotomeyo_post_path, params: {
+        parking: {
+          name: "",
+          address: "",
+          latitude: "",
+          longitude: "",
+          beginning_of_worktime: "",
+          end_of_worktime: "",
+        },
+      }
+      expect(JSON.parse(response.body)["status"]).to eq 400
+    end
   end
 end
