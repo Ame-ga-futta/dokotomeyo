f9349cdfa42c1dc3f4f8a6cd8b0cc0618971bfbb
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-12-23 13:35:52 +0900

    PostConfirm前にバリデーションが効く テスト追加

--- spec/requests/parkings_post_spec.rb
+++ spec/requests/parkings_post_spec.rb
@@ -1,32 +1,65 @@
 RSpec.describe "Parkings_post", type: :request do
   describe "POST" do
-    it "parking responce is 200" do
-      new_parking = FactoryBot.build(:parking)
-      post dokotomeyo_post_path, params: {
-        parking: {
-          name: new_parking.name,
-          address: new_parking.address,
-          latitude: new_parking.latitude,
-          longitude: new_parking.longitude,
-          beginning_of_worktime: new_parking.beginning_of_worktime,
-          end_of_worktime: new_parking.end_of_worktime,
-        },
-      }
-      expect(JSON.parse(response.body)["status"]).to eq 200
+    context "confirm" do
+      it "confirm responce is 200" do
+        new_parking = FactoryBot.build(:parking)
+        post dokotomeyo_confirm_path, params: {
+          parking: {
+            name: new_parking.name,
+            address: new_parking.address,
+            latitude: new_parking.latitude,
+            longitude: new_parking.longitude,
+            beginning_of_worktime: new_parking.beginning_of_worktime,
+            end_of_worktime: new_parking.end_of_worktime,
+          }
+        }
+        expect(JSON.parse(response.body)["status"]).to eq 200
+      end
+
+      it "confirm responce is 400" do
+        post dokotomeyo_confirm_path, params: {
+          parking: {
+            name: "",
+            address: "",
+            latitude: "",
+            longitude: "",
+            beginning_of_worktime: "",
+            end_of_worktime: "",
+          }
+        }
+        expect(JSON.parse(response.body)["status"]).to eq 400
+      end
     end
 
-    it "parking responce is 400" do
-      post dokotomeyo_post_path, params: {
-        parking: {
-          name: "",
-          address: "",
-          latitude: "",
-          longitude: "",
-          beginning_of_worktime: "",
-          end_of_worktime: "",
-        },
-      }
-      expect(JSON.parse(response.body)["status"]).to eq 400
+    context "create" do
+      it "create responce is 200" do
+        new_parking = FactoryBot.build(:parking)
+        post dokotomeyo_post_path, params: {
+          parking: {
+            name: new_parking.name,
+            address: new_parking.address,
+            latitude: new_parking.latitude,
+            longitude: new_parking.longitude,
+            beginning_of_worktime: new_parking.beginning_of_worktime,
+            end_of_worktime: new_parking.end_of_worktime,
+          },
+        }
+        expect(JSON.parse(response.body)["status"]).to eq 200
+      end
+
+      it "create responce is 400" do
+        post dokotomeyo_post_path, params: {
+          parking: {
+            name: "",
+            address: "",
+            latitude: "",
+            longitude: "",
+            beginning_of_worktime: "",
+            end_of_worktime: "",
+          },
+        }
+        expect(JSON.parse(response.body)["status"]).to eq 400
+      end
     end
   end
 end
