1e0305e3ed4a55cbabb9052f9d1ef39b791c3cad
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2022-03-07 16:14:54 +0900

    終日無料の時は他の3条件追加できないバリデーションの追加

--- app/models/requirement_buy.rb
+++ app/models/requirement_buy.rb
@@ -3,11 +3,17 @@ class RequirementBuy < ApplicationRecord
   validates :purchase_price, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :free_time, presence: true
   validates :only_weekdays, inclusion: { in: [true, false] }
-  validate :free_time_check
+  validate :free_time_check, :higher_requirement_check
 
   belongs_to :parking
 
   def free_time_check
     errors[:base] << "無料時間を入力してください" if free_time == "00:00"
+  end
+
+  def higher_requirement_check
+    if parking.requirement_frees.exists?
+      errors[:base] << "終日無料の条件と重複するため追加できません" if parking.requirement_frees.first["only_weekdays"] == only_weekdays
+    end
   end
 end
